6a09f19a6a0c232951c0d0352ae166854b60a298
James Billings <james_billings@me.com>
2019-04-03 17:56:46 -0600

    fix for prod build

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './lib/redux';
-import styled from 'styled-components';
-import InboxScreen from './components/InboxScreen';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <InboxScreen />
-      </Provider>
+      <div></div>
     );
   }
 }
